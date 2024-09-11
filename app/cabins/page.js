5c75f36b55f18471ed222a027fd8c844350dc0fb
Ruby Da Cherry <yaghoubiarshia1381@gmail.com>
2024-09-11 13:54:16 +0330

    some folder structuring

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,18 +1,10 @@
-import Navigation from "@/app/components/Navigation";
+import Navigation from "@/app/_components/Navigation";
+import Counter from "@/app/_components/Counter";
 
-async function Page() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await res.json();
-
+function Page() {
   return (
     <div>
-      <h1 className={"text-blue-400"}>Cabin&apos;s page!!</h1>
-
-      <ul>
-        {data.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      <h1 className={"text-blue-400 bg-red-400"}>Cabin&apos;s page!!</h1>
     </div>
   );
 }

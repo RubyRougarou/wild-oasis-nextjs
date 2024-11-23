9c9c34ffd5ac46c676b22f89c8bc688bb2a78027
Ruby Da Cherry <yaghoubiarshia1381@gmail.com>
2024-11-23 21:09:03 +0330

    implementing date selector and improving the interactions between server components and client components.

---
+++ app/_components/Reservation.js
@@ -0,0 +1,25 @@
+import DateSelector from "@/app/_components/DateSelector";
+import ReservationForm from "@/app/_components/ReservationForm";
+import { getBookedDatesByCabinId, getSettings } from "@/app/_lib/data-service";
+
+export default async function Reservation({ cabin }) {
+  const [settings, bookedDates] = await Promise.all([
+    getSettings(),
+    getBookedDatesByCabinId(cabin.id),
+  ]);
+
+  return (
+    <div
+      className={
+        "flex justify-between border border-primary-800 h-auto w-[75rem] p-2"
+      }
+    >
+      <DateSelector
+        settings={settings}
+        bookedDates={bookedDates}
+        cabin={cabin}
+      />
+      <ReservationForm cabin={cabin} />
+    </div>
+  );
+}

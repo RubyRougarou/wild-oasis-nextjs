5b047cf39f2900650591b7662ee42efe8e334d4e
Ruby Da Cherry <yaghoubiarshia1381@gmail.com>
2024-11-24 15:35:39 +0330

    implemented an API endpoint using next.js route handler

---
+++ app/api/cabins/[cabinId]/route.js
@@ -0,0 +1,17 @@
+// we can do this for all HTTP verbs like PUT,POST,PATCH,HEAD and OPTIONS.
+
+import { getBookedDatesByCabinId, getCabin } from "@/app/_lib/data-service";
+
+export async function GET(request, { params }) {
+  const { cabinId } = params;
+
+  try {
+    const [cabin, bookedDates] = await Promise.all([
+      getCabin(cabinId),
+      getBookedDatesByCabinId(cabinId),
+    ]);
+    return Response.json({ cabin, bookedDates });
+  } catch (error) {
+    return Response.json({ message: "cabin not found." });
+  }
+}

d142069b39c5619207e51847f40166e9b9ddad78
Ruby Da Cherry <yaghoubiarshia1381@gmail.com>
2024-11-27 16:53:02 +0330

    implemented authentication with Google using Auth.js

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,6 +1,9 @@
 import Link from "next/link";
+import { auth } from "@/app/_lib/auth";
 
-export default function Navigation() {
+export default async function Navigation() {
+  const session = await auth(); // this makes the entire route dynamic
+
   return (
     <nav className={"z-10 text-xl"}>
       <ul className={"flex gap-16 items-center"}>
@@ -21,12 +24,29 @@ export default function Navigation() {
           </Link>
         </li>
         <li>
-          <Link
-            href={"/account"}
-            className={"hover:text-accent-400 transition-colors"}
-          >
-            Guest area
-          </Link>
+          {session?.user?.image ? (
+            <Link
+              href={"/account"}
+              className={
+                "hover:text-accent-400 transition-colors flex items-center gap-4"
+              }
+            >
+              <span>Guest area</span>
+              <img
+                className={"h-9 rounded-full"}
+                src={session.user.image}
+                alt="user avatar"
+                referrerPolicy={"no-referrer"}
+              />
+            </Link>
+          ) : (
+            <Link
+              href={"/account"}
+              className={"hover:text-accent-400 transition-colors"}
+            >
+              Guest area
+            </Link>
+          )}
         </li>
       </ul>
     </nav>

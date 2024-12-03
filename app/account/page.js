7dca941dc512155c7b9fc7362267a94652bd8e91
Ruby Da Cherry <yaghoubiarshia1381@gmail.com>
2024-12-03 11:21:21 +0330

    implemented middleware for authorization, customized login page and signIn and signOut features.

--- app/account/page.js
+++ app/account/page.js
@@ -1,12 +1,18 @@
+import { auth } from "@/app/_lib/auth";
+
 export const metadata = {
   title: "Guests area",
 };
 
-function Page() {
+async function Page() {
+  const session = await auth();
+  // to get the first name:
+  const firstName = session.user.name.split(" ").at(0);
+
   return (
-    <div>
-      <h1>Welcome User</h1>
-    </div>
+    <h2 className={"font-semibold text-2xl text-accent-400 mb-7"}>
+      Welcome, {session.user.name}
+    </h2>
   );
 }
 
